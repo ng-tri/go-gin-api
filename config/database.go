bb3a9cc681a56aee4b90dc59ee52a2a701982e25
ng-tri <changelle24@gmail.com>
2025-06-27 15:39:31 +0700

    connection with database use mysql

--- config/database.go
+++ config/database.go
@@ -11,8 +11,22 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
+	// mysql
+	// password := os.Getenv("DB_PASSWORD")
+	// dsn := fmt.Sprintf("root:%s@tcp(127.0.0.1:3306)/go_gin_api?charset=utf8mb4&parseTime=True&loc=Local", password)
+
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
+	// dsn := os.Getenv("DSN")
+	// fmt.Println(dsn)
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// postgres
 	dsn := "host=localhost user=postgres password=newpassword dbname=go_gin_api port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
 	if err != nil {
 		log.Fatal("Failed to connect to DB: ", err)
 	}
--- models/product.go
+++ models/product.go
@@ -1,6 +1,12 @@
 package models
 
 type Product struct {
+	// mysql, gorm sẽ tự tạo cột ID, CreatedAt, UpdatedAt, DeletedAt
+	// gorm.Model
+	// Name  string `json:"name"`
+	// Price int    `json:"price"`
+
+	// postgres
 	ID    uint   `gorm:"primaryKey" json:"id"`
 	Name  string `json:"name" binding:"required,min=2"`
 	Price int    `json:"price" binding:"required,gt=0"`
