9acc8d44095f2566d3bb7e37bf9b31543a93d6c9
ng-tri <changelle24@gmail.com>
2025-07-02 16:16:54 +0700

    middleware log request

---
+++ middlewares/logger.go
@@ -0,0 +1,20 @@
+package middlewares
+
+import (
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		c.Next() // tiếp tục xử lý route
+
+		latency := time.Since(start)
+		status := c.Writer.Status()
+
+		log.Printf("[GIN] %d | %v | %s %s\n", status, latency, c.Request.Method, c.Request.URL.Path)
+	}
+}
