cb975be1fbe221e70f6d8892cf65544884a1ab02
ng-tri <changelle24@gmail.com>
2025-12-10 15:33:11 +0700

    update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"go-gin-api/internal/database"
 	"go-gin-api/internal/middleware"
 	"go-gin-api/internal/model"
@@ -10,17 +12,30 @@ import (
 )
 
 func main() {
+	// DATABASE INIT
 	database.ConnectDB()
-	database.DB.AutoMigrate(&model.Product{})
-	gin.SetMode(gin.ReleaseMode)
+	database.DB.AutoMigrate(
+		&model.User{},
+		&model.Product{},
+		&model.Order{},
+		&model.OrderItem{},
+	)
 
+	// GIN INIT
+	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
+
 	r.Use(gin.Logger(), gin.Recovery())
 	r.Use(middleware.LoggerMiddleware())
 	r.SetTrustedProxies([]string{"127.0.0.1"})
 
+	// REGISTER DOMAIN ROUTES
 	route.RegisterAuthRoutes(r)
+	route.RegisterOrderRoutes(r)
 	route.RegisterProductRoutes(r)
+	route.RegisterUserRoutes(r)
 
+	// RUN SERVER at 8080
+	fmt.Println("API Server running at :8080")
 	r.Run(":8080")
 }
